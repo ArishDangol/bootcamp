a8edb4b3235dac0fdfbcdf7662c50ddcf3ba9576
ArishDangol <96335887+ArishDangol@users.noreply.github.com>
2021-12-24 09:26:54 +0545

    add js

---
+++ registration.js
@@ -0,0 +1,32 @@
+console.log("this is registration page");
+
+const registration = document.getElementById("registration-form");
+
+console.log(registration);
+
+function getSelected(select){
+    let selectedValues= []
+    for (optionIndex in select){
+        if (select.option[optionIndex].selected){
+            selectedValues.push(select.options[optionIndex].value);
+        }
+    }
+    return selectedValues;
+}
+registration.onsubmit = function(event){
+    event.preventDefault();
+    console.log("I am triggered when submit is pressed");
+    let name  = document.getElementById("name").value;
+    let interest  = getSelected(document.getElementById("interests"));
+    let dob = document.getElementById("dob").value;
+    let darkMode = document.getElementById("dark-mode").checked;
+    const data = {
+        name  : name,
+        interest : interest,
+        dob : dob,
+        darkMode : darkMode,
+        
+    };
+    console.log(data);
+};
+
